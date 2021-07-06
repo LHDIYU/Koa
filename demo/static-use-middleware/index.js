51143cc3f2da4785b4644fcceefc43c814876f12
罗焕 <luohuan@gaosiedu.com>
2021-07-07 01:07:50 +0800

    feat: cache

---
+++ demo/static-use-middleware/index.js
@@ -0,0 +1,28 @@
+const Koa = require('koa')
+const path = require('path')
+//静态资源中间件
+const static = require('koa-static')
+
+const app = new Koa()
+
+// 静态资源目录对于相对入口文件index.js的路径
+const staticPath = './static'
+
+
+
+app.use( async ( ctx, next ) => {
+   // 设置响应头Cache-Control 设置资源有效期为300秒
+   ctx.set({
+    'Cache-Control': 'max-age=300'  
+  });
+  await next();
+})
+
+app.use(static(
+  path.join( __dirname,  staticPath)
+))
+
+app.listen(3000, () => {
+  console.log('[demo] static-use-middleware is starting at port 3000')
+})
+
